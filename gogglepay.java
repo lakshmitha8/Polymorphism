9647c369651ba7149951dc4c66d2905da2e02960
lakshmitha8 <89855709+lakshmitha8@users.noreply.github.com>
2021-10-27 07:25:58 +0530

    Add files via upload

---
+++ gogglepay.java
@@ -0,0 +1,18 @@
+package com.Xworkz.polymorphism;
+
+public class gogglepay {
+
+	
+	public void billpayment(int no,int amount)
+	{
+		System.out.println("DTH recharge succesfull");
+		
+	}
+	
+	public void recharge(long no, int amount)
+	{
+		System.out.println("electricty bill payment succesfull");
+		
+	}
+
+}
---
+++ phonepay.java
@@ -0,0 +1,17 @@
+package com.Xworkz.polymorphism;
+
+public class phonepay {
+
+	public void recharge(int userno,int amount)
+	{
+		System.out.println("DTH recharge succesfull");
+		
+	}
+	
+	public void recharge(long mobileno,int amount)
+	{
+		System.out.println("mobile recharge succesfull");
+		
+	}
+
+}
